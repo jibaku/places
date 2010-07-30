1d23e60c62a16559924811b238a5d48779e715d4
Fabien Schwob <github@x-phuture.com>
2010-07-30 18:20:38 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+ 
+setup(
+    name = "places",
+    version = "0.1",
+    author = "Fabien Schwob",
+    author_email = "fabien@x-phuture.com",
+    license = "BSD",
+    url = "http://bitbucket.org/jibaku/places",
+    packages = [
+        "places",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)

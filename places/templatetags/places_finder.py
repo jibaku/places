4cef4ec05c88489e1fc192e077f4c97c6ba4f54c
Fabien Schwob <github@x-phuture.com>
2010-08-06 23:14:18 +0200

    added a template tag to retrieve places

---
+++ places/templatetags/places_finder.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from django import template
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+register = template.Library()
+
+from places.models import Place
+
+class PlacesNode(template.Node):
+    def __init__(self, var_name, filters):
+        self.var_name = var_name
+        self.filters = filters
+
+    def render(self, context):
+        context[self.var_name] = Place.objects.filter(site__id=settings.SITE_ID).order_by('name')
+        return ''
+
+
+@register.tag(name="places_filter")
+def places_filter(parser, token):
+    """
+    {% places_filter <filters> as places %}
+    """
+    tokens = token.split_contents()
+    raise_error = False
+    if len(tokens) == 4:
+        if tokens[0] == 'places_filter' and tokens[2] == 'as':
+            var_name = tokens[3]
+            filters = tokens[1]
+        else:
+            raise_error = True
+    elif len(tokens) == 3:
+        if tokens[0] == 'places_filter' and tokens[1] == 'as':
+            var_name = tokens[2]
+            filters = None
+        else:
+            raise_error = True
+    
+    if raise_error:
+        raise template.TemplateSyntaxError, "%r tag must be used with %s" % (tokens[0], "{% categories as categories %}")
+    else:    
+        return PlacesNode(var_name, filters)

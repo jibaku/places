96f4b3cadbf9f528c55096eb8fac536fd1410a6b
Fabien Schwob <github@x-phuture.com>
2010-08-06 14:52:50 +0200

    small enhancements

--- places/models.py
+++ places/models.py
@@ -7,14 +7,19 @@ class Place(models.Model):
     site = models.ForeignKey(Site, default=settings.SITE_ID)
     name = models.CharField(max_length=200)
     slug = models.SlugField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    longitude = models.FloatField(blank=True)
+    latitude = models.FloatField(blank=True)
     
     street = models.CharField(blank=True, max_length=250)
     postal_code = models.CharField(blank=True, max_length=25)
     city = models.CharField(blank=True, max_length=100)
     
     description = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = (
+            ('site', 'slug')
+        )
     
     def __unicode__(self):
         return self.name
--- places/urls.py
+++ places/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-import settings
 
 urlpatterns = patterns('places.views',
     url(r'^(?P<place_slug>[-\w]+)/$', 'place_detail', name='places-detail'),

c768dc52b4d4dd7f8644c1f9078fa88baffb4f32
Fabien Schwob <github@x-phuture.com>
2010-08-18 18:27:21 +0200

    Added a view to list the places

--- places/urls.py
+++ places/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('places.views',
+    url(r'^$', 'place_list', name='places-index'),
     url(r'^(?P<place_slug>[-\w]+)/$', 'place_detail', name='places-detail'),
 )
--- places/views.py
+++ places/views.py
@@ -1,6 +1,10 @@
-from django.views.generic.list_detail import object_detail
+from django.views.generic.list_detail import object_detail, object_list
 from places.models import Place
 from django.conf import settings
+
+def place_list(request):
+    queryset = Place.objects.filter(site__id=settings.SITE_ID)
+    return object_list(request, queryset=queryset)
 
 def place_detail(request, place_slug):
     queryset = Place.objects.filter(site__id=settings.SITE_ID)
